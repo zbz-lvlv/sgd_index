f2222d7ec610afb91ff3e5c76a9c71ae45517ee0
Zhang Bozheng <zbz.lvlv@gmail.com>
2017-10-21 21:30:41 +0800

    Finished project

--- sgd_index.py
+++ sgd_index.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from dateutil import parser
 import pandas as pd
 import pandas_datareader.data as web
 
@@ -7,7 +8,6 @@ tickers = ['SGDUSD=X',
            'SGDCNY=X',
            'SGDJPY=X',
            'SGDGBP=X',
-           'SGDINR=X',
            'SGDBRL=X',
            'SGDCAD=X',
            'SGDRUB=X',
@@ -18,26 +18,126 @@ tickers = ['SGDUSD=X',
            'SGDCHF=X',
            'SGDSAR=X']
 
-def getFinancialData(startDate, endDate, ticker):
-    df = web.DataReader(ticker, 'yahoo', startDate, endDate);
-    return df['Adj Close']
+countries = ['US',
+             'EU',
+             'CN',
+             'JP',
+             'GB',
+             'BR',
+             'CA',
+             'RU',
+             'AU',
+             'MX',
+             'ID',
+             'TR',
+             'CH',
+             'SA']
 
-def getIndividualCurrencyChanges(rawData):
-    return rawData / rawData.shift(1);
+def getGDPData(country):
+    df = pd.read_csv("data\\gdp\\" + country + ".csv", index_col = 1, names = ['Country', 'GDP']);
+    return df
 
-def getWeightedCurrencyChanges(changes, gdps):
-    pass
+def getForexData(ticker):
+    df = pd.read_csv("data\\forex\\" + ticker + ".csv", index_col = 0, names = ['Price', 'Change']);
+    return df
 
-for i in range(0, len(tickers)):
+def getIndexChanges(forex, gdp):
     
-    try:
-        rawData = getFinancialData(dt.datetime(2007, 10, 21), dt.datetime(2017, 10, 20), tickers[i]);
-        changes = getIndividualCurrencyChanges(rawData);
-    except Exception: 
-        pass
+    gdp.drop(gdp.tail(1).index,inplace=True)
+    forex.drop(forex.tail(1).index,inplace=True)
     
-    rawData = getFinancialData(dt.datetime(2007, 10, 21), dt.datetime(2017, 10, 20), tickers[i]);
-    changes = getIndividualCurrencyChanges(rawData);
-    print(rawData.tail())
+    indexChanges = []
+    
+    #Loop through every day
+    for i in range(0, len(forex)):
+        
+        dateTime = parser.parse(forex.index.values[i])
+        year = dateTime.year;
+        if year == 2017:
+            year = 2016
+        
+        indexChanges.append(calculateWeightedGeometricMean(forex.iloc[i], gdp.loc[str(year)]))
+        
+    return indexChanges
+
+def calculateWeightedGeometricMean(forex, gdp):
+    
+    a = 1;
+    
+    #Loop through every country/currency
+    for i in range(0, len(forex)):
+        
+        #clean data  
+        try:
+            val = float(forex.iloc[i])
+        except ValueError:
+            forex.iloc[i] = '1'
+            
+        if forex.iloc[i] == '0':
+            forex.iloc[i] = '1';
+        
+        try:
+            val = float(gdp.iloc[i])
+        except ValueError:
+            gdp.iloc[i] = '1'
+            
+        if gdp.iloc[i] == '0':
+            gdp.iloc[i] = '1';
+        
+        a = a * pow(float(forex.iloc[i]), float(gdp.iloc[i]) / 10000000000.0) #divide 10,000,000,000 ten billion
+        
+    a = pow(a, 1 / calculateSumOfGDP(gdp))
+    
+    return a
+        
+def calculateSumOfGDP(gdps):
+    
+    a = 0.0
+    
+    for gdp in gdps:
+        a = a + float(gdp)
+        
+    return a / 10000000000.0
+
+def getIndex(indexChanges):
+    
+    indices = []
+    index = 100.0
+    
+    for i in range(0, len(indexChanges)):
+        
+        #Clean data
+        if indexChanges[i] != indexChanges[i]:
+            indexChanges[i] = 1.0
+        
+        index = index * indexChanges[i]
+        indices.append(index)
+        
+    return indices
+    
+
+gdpAllDf = pd.DataFrame()
+forexAllDf = pd.DataFrame()
+
+for i in range(0, len(countries)):
+    
+    gdpDf = getGDPData(countries[i])
+    gdpDf = gdpDf.iloc[::-1] #Reverse, to be in ascending date
+    forexDf = getForexData(tickers[i])
+    
+    gdpAllDf[countries[i]] = gdpDf['GDP'];
+    forexAllDf[tickers[i]] = forexDf['Change'];
+    
+indexChanges = getIndexChanges(forexAllDf, gdpAllDf)
+indices = getIndex(indexChanges)
+
+df = pd.DataFrame(indices);
+df.to_csv('SGD_Index.csv');
+    
+    
+    
+    
+    
+   
 
 
